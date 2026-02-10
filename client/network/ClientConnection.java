fd0e993b6a4120ae06e6f759134e65c07991acbf
mohamadhoseinsamadi <msmdy7194@gmail.com>
2026-02-11 00:15:49 +0330

    feat: implement client-server socket connection

--- client/network/ClientConnection.java
+++ client/network/ClientConnection.java
@@ -1,5 +1,48 @@
-﻿package client.network.ClientConnection;
+﻿package client.network;
+
+import shared.message.Message;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class ClientConnection {
-}
+    private static ClientConnection instance;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
+    private ClientConnection() {}
+
+    public static ClientConnection getInstance() {
+        if (instance == null)
+            instance = new ClientConnection();
+        return instance;
+    }
+
+    public void connect(String host, int port) throws Exception {
+        Socket socket = new Socket(host, port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message msg) throws Exception {
+        out.writeObject(msg);
+        out.flush();
+    }
+
+    public Message receive() throws Exception {
+        return (Message) in.readObject();
+    }
+
+    public void startListening(MessageListener listener) {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    Message msg = receive();
+                    listener.onMessage(msg);
+                }
+            } catch (Exception e) {
+                System.out.println("Connection lost");
+            }
+        }).start();
+    }
+}
