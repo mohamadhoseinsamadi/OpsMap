7cb3af8c8764303b7f101193d20c085094e02902
Hellix <heliyya.hb.bgh.2005@gmail.com>
2026-02-12 23:10:50 +0330

    Update UserStore.java

--- server/storage/UserStore.java
+++ server/storage/UserStore.java
@@ -1,16 +1,40 @@
-﻿package server.storage.UserStore;
+package server.storage;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserStore {
-    private Set<String> users = new HashSet<>();
+    private static class UserRecord {
+        String username;
+        String password;
+        String role;
 
-    public boolean userExists(String username) {
-        return users.contains(username);
+        UserRecord(String username, String password, String role) {
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
     }
 
-    public void addUser(String username) {
-        users.add(username);
+    private Map<String, UserRecord> users = new HashMap<>();
+
+    public boolean userExists(String username) {
+        return users.containsKey(username);
+    }
+
+    public boolean validate(String username, String password) {
+        UserRecord r = users.get(username);
+        if (r == null) return false;
+        return r.password.equals(password);
+    }
+
+    public void addUser(String username, String password, String role) {
+        users.put(username, new UserRecord(username, password, role));
+    }
+
+   
+    public String getRole(String username) {
+        UserRecord r = users.get(username);
+        return r != null ? r.role : null;
     }
 }
