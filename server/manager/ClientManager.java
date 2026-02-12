36ea9a652ebfb8f8d9cdc3097bd29034b4bdb312
Hellix <heliyya.hb.bgh.2005@gmail.com>
2026-02-12 23:08:18 +0330

    Update ClientManager.java

--- server/manager/ClientManager.java
+++ server/manager/ClientManager.java
@@ -1,11 +1,14 @@
-﻿package server.manager.ClientManager;
+package server.manager;
 
 import server.ClientHandler;
+import shared.model.User;
 import shared.message.Message;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientManager {
     private Set<ClientHandler> clients = Collections.synchronizedSet(new HashSet<>());
@@ -24,10 +27,24 @@ public class ClientManager {
                 if (c != exclude) {
                     try {
                         c.send(msg);
-                    } catch (IOException ignored) {
+                    } catch (IOException e) {
+                    
                     }
                 }
             }
         }
+    }
+
+    public List<User> getUsers() {
+        List<User> list = new ArrayList<>();
+        synchronized (clients) {
+            for (ClientHandler c : clients) {
+                User u = c.getUser();
+                if (u != null) {
+                    list.add(u);
+                }
+            }
+        }
+        return list;
     }
 }
