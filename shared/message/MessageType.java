e9bc6c3bbfd73d3bed0895778f97fc644b6f2bd0
mohamadhoseinsamadi <msmdy7194@gmail.com>
2026-02-13 17:49:49 +0330

    feat(shared-message): Define MessageType enum for all message types

---
+++ shared/message/MessageType.java
@@ -0,0 +1,21 @@
+package shared.message;
+
+public enum MessageType {
+    LOGIN,
+    REGISTER,
+    LOGIN_SUCCESS,
+    LOGIN_FAILED,
+    DRAW_ROUTE,
+    ADD_MARKER,
+    ADD_REGION,
+    REMOVE_OBJECT,
+    MOUSE_MOVE,
+    USER_JOINED,
+    USER_LEFT,
+    USER_LIST,
+    CHAT,
+    ERROR,
+    SAVE_STATE,
+    LOAD_STATE,
+    MAP_STATE
+}
